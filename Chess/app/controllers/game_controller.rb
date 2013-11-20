7d51c2cd2a4c8c8a2c9979a76e5b1a3879bd7e02
App Academy Student <student@appacademy.io>
2013-11-20 17:10:28 -0500

    #move request

--- Chess/app/controllers/game_controller.rb
+++ Chess/app/controllers/game_controller.rb
@@ -3,7 +3,6 @@ class GameController < ApplicationController
   def find
     player = nil
     if session[:token].nil?
-      #new player
       player = Player.create
       session[:token] = player.session_token
     else
@@ -13,13 +12,12 @@ class GameController < ApplicationController
       player.last_request = Time.now
       player.save
       if player.game_id.nil?
-        time_before_expires = 10.seconds.ago
+        time_before_expires = 30.seconds.ago
         players = Player.where("last_request > ? AND game_id IS NULL", time_before_expires)
-
         if players.count >= 2
           game = Game.create(black_id: players.first.id, white_id: players.second.id, turn: :black)
-          game.black.update_attributes(game_id: game.id, color: 'BLACK')
-          game.white.update_attributes(game_id: game.id, color: 'WHITE')
+          game.black.update_attributes(game_id: game.id, color: :black)
+          game.white.update_attributes(game_id: game.id, color: :white)
           render json: { game: game, your_player_id: player.id }
         else
           render json: {status: "Waiting for another player still..."}
@@ -27,6 +25,18 @@ class GameController < ApplicationController
       else
         render json: { game: player.game, your_player_id: player.id }
       end
+    end
+  end
+
+  def move
+    player = Player.find_by_session_token(session[:token])
+    if player.nil? || player.game_id.nil? || player.game.turn != player.color
+      render json: { error: true, message: "It's not your turn or you're not apart of this game"}
+    elsif params[:start_pos].nil? || params[:end_pos].nil?
+      render json: { error: true, message: "You didn't provide sufficient move coordinates"}
+    else
+      player.moves.create(start_pos: params[:start_pos], end_pos: params[:end_pos])
+      render json: { game: player.game, your_player_id: player.id }
     end
   end
 end
